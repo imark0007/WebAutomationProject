510a0800542cd160a26a44511d08cf5d7610cbb5
Arka Talukder <arkatalukder99@gmail.com>
2024-04-20 21:55:52 +0600

    initial commit webAutomation

--- src/test/java/org/example/HandleBrowser.java
+++ src/test/java/org/example/HandleBrowser.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HandleBrowser {
    @Test
     public  void workWithBrowser() throws InterruptedException {
@@ -50,6 +54,35 @@ public class HandleBrowser {
             driver.get("https://rahulshettyacademy.com/AutomationPractice/");
             driver.findElement(By.xpath("//*[text()='Open Window']")).click();
             Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[text()='Open Tab']")).click();
+            Thread.sleep(2000);
+            System.out.println(driver.getWindowHandles());
+            List<String> window_handles = new ArrayList<>(driver.getWindowHandles());
+            for (String handle: window_handles){
+                System.out.println(handle);
+            }
+
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+            System.out.println("++++++++++++++++++++++++++++");
+            driver.switchTo().window(window_handles.get(1));
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+            System.out.println("++++++++++++++++++++++++++++");
+            driver.switchTo().window(window_handles.get(0));
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+            System.out.println("++++++++++++++++++++++++++++");
+            driver.switchTo().window(window_handles.get(2));
+            System.out.println(driver.getTitle());
+            System.out.println(driver.getCurrentUrl());
+
+            driver.quit();
+
+
+
+
+
         }
 
 
